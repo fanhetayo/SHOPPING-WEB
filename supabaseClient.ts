a95c205fa2ecc7519eae08714fe19248b1f8c918
fanhetayo <147045571+fanhetayo@users.noreply.github.com>
2026-03-04 21:06:20 +0700

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iisexzdhptmhpzwpfenz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlpc2V4emRocHRtaHB6d3BmZW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MDU5NTcsImV4cCI6MjA4ODA4MTk1N30.WQPQ5GU9ddcg5TWLWBgjLebW0sXoWPjOfo-cgRfgSTU';
+const supabaseUrl = 'https://hkhajycblrfslxnapogx.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhraGFqeWNibHJmc2x4bmFwb2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2MjY5MTcsImV4cCI6MjA4ODIwMjkxN30.wtyi1Cn3nXBWHdOhQJ9InFiJwvdZUsE_ZMT8jb2PI54';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
