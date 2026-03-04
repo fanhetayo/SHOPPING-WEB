f0bdcef31e79f02e26a98ec57e500c25c67851c3
fanhetayo <147045571+fanhetayo@users.noreply.github.com>
2026-03-04 21:38:21 +0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,18 +1,19 @@
+// main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Admin from './Admin'
-import './index.css'
+
+// PASTIKAN BARIS INI BENAR
+// Jika file index.css ada di folder yang sama dengan main.tsx
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Halaman Toko Utama */}
         <Route path="/" element={<App />} />
-        
-        {/* Halaman Admin Panel sesuai permintaan */}
         <Route path="/backoffice" element={<Admin />} />
       </Routes>
     </BrowserRouter>
