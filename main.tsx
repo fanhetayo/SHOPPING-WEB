c0231238f1f231a5839204e530c7608876239960
fanhetayo <147045571+fanhetayo@users.noreply.github.com>
2026-03-04 21:33:40 +0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Admin from './Admin';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Admin from './Admin'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* Halaman Toko Utama */}
         <Route path="/" element={<App />} />
-        <Route path="/backoffice-secret" element={<Admin />} />
+        
+        {/* Halaman Admin Panel sesuai permintaan */}
+        <Route path="/backoffice" element={<Admin />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
