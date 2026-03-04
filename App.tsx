82bcfb8a7d2073a8c4aaf21ade3aaabb8eb7a7a2
fanhetayo <147045571+fanhetayo@users.noreply.github.com>
2026-03-04 23:44:31 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -17,15 +17,15 @@ export default function App() {
 
   // --- IMPLEMENTASI TAWK.TO ---
   useEffect(() => {
-    var Tawk_API: any = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function(){
-      var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/69a85b337b02b21c3601f237/1jisq8guj';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin', '*');
-      s0.parentNode?.insertBefore(s1, s0);
-    })();
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69a85b337b02b21c3601f237/1jisr726s';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
   }, []);
 
   useEffect(() => {
